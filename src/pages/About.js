d2bb3d46a912e9f40eb898ef6e3fd09bc1cb79e2
Jerry Simon <seryjemon@gmail.com>
2022-06-19 00:47:28 -0600

    week 2

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
+import profile_pic from "../assets/profile.jpg";
 
 export default class About extends Component {
   render() {
@@ -12,16 +12,20 @@ export default class About extends Component {
             <img
               className="profile_image"
               src={profile_pic}
-              alt="Profile Pic"
+              alt="Profile"
             ></img>
           </div>
         </div>
         <div className="split right">
           <div className="centered">
-            <div className="name_title">Your Name</div>
+            <div className="name_title">Jerr Simon</div>
             <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+            I'm a native of Ghana but resides in Rexburg, Idaho. Currently a Senior at Brigham Young University Idaho,
+            majoring in Computer Engineering. I didn't have the privillege of experiencing technology growing up. Despite that,
+            I began to develop a curios mindset about how computing devices functioned. In pursuit of my curiosity, I ended up majoring in Computer Engineering,
+            which has exposed me to both the hardware and software aspect of computing systems. Although challenging and complex, I have come to 
+            develop a passion for smart computing technology. During my leisure, I like going on hiking, playing soccer, 
+            gym workout, snowboarding, water rafting, and many more.
             </div>
           </div>
         </div>
